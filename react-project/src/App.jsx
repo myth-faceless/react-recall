a0672afb49ebadc1e241df859ddd6bcb9981e1b1
myth-faceless <mythmanish743@gmail.com>
2025-04-07 21:45:50 +0545

    handle necessary changes

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,11 @@
+import MovieCard from "./components/MovieCard.jsx";
 import ProfileCard from "./components/ProfileCard.jsx";
 const App = () => {
   return (
     <div>
       <h1> Hello Manish, Learn React.js !</h1>
-      <ProfileCard />
+      {/* <ProfileCard /> */}
+      <MovieCard />
     </div>
   );
 };
---
+++ react-project/src/assets/movieAPI.js
@@ -0,0 +1,122 @@
+export const movieAPI = [
+  {
+    id: 1,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    name: "The Shawshank Redemption",
+    ratings: 9.3,
+    summary:
+      "Two imprisoned men bond over years, finding solace and eventual redemption through acts of decency.",
+    genre: ["Drama"],
+    cast: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+    movielink: "https://www.imdb.com/title/tt0111161/",
+  },
+  {
+    id: 2,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    name: "Inception",
+    ratings: 8.8,
+    summary:
+      "A thief who steals corporate secrets through dream-sharing is given the inverse task of planting an idea.",
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    cast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+    movielink: "https://www.imdb.com/title/tt1375666/",
+  },
+  {
+    id: 3,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/iVZ3JAcAjmguGPnRNfWFOtLHOuY.jpg",
+    name: "The Godfather",
+    ratings: 9.2,
+    summary:
+      "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his son.",
+    genre: ["Crime", "Drama"],
+    cast: ["Marlon Brando", "Al Pacino", "James Caan"],
+    movielink: "https://www.imdb.com/title/tt0068646/",
+  },
+  {
+    id: 4,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    name: "The Dark Knight",
+    ratings: 9.0,
+    summary:
+      "Batman faces the Joker, a criminal mastermind who seeks to create chaos in Gotham.",
+    genre: ["Action", "Crime", "Drama"],
+    cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    movielink: "https://www.imdb.com/title/tt0468569/",
+  },
+  {
+    id: 5,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg",
+    name: "Pulp Fiction",
+    ratings: 8.9,
+    summary:
+      "The lives of two mob hitmen, a boxer, and others intertwine in a tale of violence and redemption.",
+    genre: ["Crime", "Drama"],
+    cast: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+    movielink: "https://www.imdb.com/title/tt0110912/",
+  },
+  {
+    id: 6,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
+    name: "Fight Club",
+    ratings: 8.8,
+    summary:
+      "An insomniac office worker and a soap maker form an underground fight club that evolves into something more.",
+    genre: ["Drama"],
+    cast: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
+    movielink: "https://www.imdb.com/title/tt0137523/",
+  },
+  {
+    id: 7,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/saHP97rTPS5eLmrLQEcANmKrsFl.jpg",
+    name: "Forrest Gump",
+    ratings: 8.8,
+    summary:
+      "The life journey of Forrest Gump, a man with a low IQ, who influences major historical events.",
+    genre: ["Drama", "Romance"],
+    cast: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
+    movielink: "https://www.imdb.com/title/tt0109830/",
+  },
+  {
+    id: 8,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+    name: "The Matrix",
+    ratings: 8.7,
+    summary:
+      "A computer hacker learns about the true nature of his reality and his role in the war against its controllers.",
+    genre: ["Action", "Sci-Fi"],
+    cast: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+    movielink: "https://www.imdb.com/title/tt0133093/",
+  },
+  {
+    id: 9,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    name: "Interstellar",
+    ratings: 8.6,
+    summary:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    genre: ["Adventure", "Drama", "Sci-Fi"],
+    cast: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+    movielink: "https://www.imdb.com/title/tt0816692/",
+  },
+  {
+    id: 10,
+    cover_image:
+      "https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg",
+    name: "The Lord of the Rings: The Return of the King",
+    ratings: 9.0,
+    summary:
+      "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam.",
+    genre: ["Action", "Adventure", "Drama"],
+    cast: ["Elijah Wood", "Viggo Mortensen", "Ian McKellen"],
+    movielink: "https://www.imdb.com/title/tt0167260/",
+  },
+];
